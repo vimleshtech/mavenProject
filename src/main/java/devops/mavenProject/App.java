b6b6186223c2830582cf66ece7c6734a4f1bd83e
saurabh <gl_saurabh@yahoo.com>
2018-03-10 13:32:35 +0530

    test

--- src/main/java/devops/mavenProject/App.java
+++ src/main/java/devops/mavenProject/App.java
@@ -8,6 +8,12 @@ public class App
 {
     public static void main( String[] args )
     {
+    	String a ="this is java program";
+    	String c[] = a.split(" ");
+    	System.out.println( c.length);
+    	
+    	//or
+    	//System.out.println( a.length()-  a.replace(" ", "").length());
         System.out.println( "Hello World!" );
     }
 }
